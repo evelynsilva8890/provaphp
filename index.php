96d986743476487d2f6a6dd33683033e65de76af
evelynsilva8890 <evelyn889067@gmail.com>
2024-12-06 15:35:14 -0300

    provaphp

---
+++ index.php
@@ -0,0 +1,46 @@
+<h2>receita:
+    <?php
+    echo "chocolate quente";?>
+    </h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ingredientes</th>
+                <th>quantidade</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>leite</td>
+                <td>1 litro</td>
+            </tr>
+            <tr>
+                <td>amido de milho</td>
+                <td>1 colher de sopa</td>
+            </tr>
+            <tr>
+                <td>chocolate meio amargo</td>
+                <td>150g</td>
+            </tr>
+            <tr>
+                <td> leite em pó</td>
+                <td>50g</td>
+            </tr>
+            <tr>
+                <td>canela</td>
+                <td>1 colher de chá</td>
+            </tr>
+            <tr>
+                <td>creme de leite fresco</td>
+                <td>1 xicara</td>
+            </tr>
+            <tr>
+                <td>açucar</td>
+                <td>1/4 de xicara</td>
+            </tr>
+        </tbody>
+    <h1>nota:</h1>
+    <script>
+    var h1=document.querySelector("h1");
+    h1.innerHTML="nota:10";
+    </script>
